879fb5961a66a4cbfbf2572253f558a82aa6c6dc
shrikrishnaambekar <145032478+shrikrishnaambekar@users.noreply.github.com>
2025-12-22 16:56:31 +0530

    Add files via upload

---
+++ 8b/8b.sql
@@ -0,0 +1,9 @@
+show databases;
+
+create database students124;
+use students124;
+create table students124 (id int primary key , name varchar(50) , grade float);
+
+insert into students124 values (1,"ah",99);
+insert into students124 values (2,"k",90);
+insert into students124 values (3,"S",95);
